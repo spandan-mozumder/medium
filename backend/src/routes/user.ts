166bcf36c9ebf589076a53cbdc59c0a3240f28da
spandan-mozumder <mozumder.spandan.professional@gmail.com>
2025-05-16 16:58:44 +0530

    first commit

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,78 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { sign } from "hono/jwt";
+import { signupInput, signinInput } from "@spandyzlost/medium-zod-types";
+
+export const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+userRouter.post("/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const { success } = signupInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({ error: "Invalid Input" });
+  }
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: body.username,
+        password: body.password,
+        name: body.name,
+      },
+    });
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+    return c.json({
+      jwt: token,
+    });
+  } catch (e) {
+    c.status(401);
+    return c.json({ error: "User Already Exists" });
+  }
+});
+
+userRouter.post("/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const { success } = signinInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({ error: "Invalid Input" });
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: body.username,
+        password: body.password,
+      },
+    });
+
+    if (!user) {
+      c.status(403);
+      return c.json({ error: "User Not Found" });
+    }
+
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt: token });
+  } catch (e) {
+    c.status(411);
+    return c.json({ error: "Invalid Credentials" });
+  }
+});
