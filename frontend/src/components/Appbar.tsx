b406690c49ec0f1e122f2edc74a72e481bff0620
spandan-mozumder <mozumder.spandan.professional@gmail.com>
2025-05-28 13:03:39 +0530

    appbar update

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -11,45 +11,47 @@ export const Appbar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-between border-b px-10 py-4">
+    <div>
       <ToastContainer />
-      <Link
-        to={'/blogs'}
-        className="text-bold flex cursor-pointer flex-col justify-center text-3xl"
-      >
-        Medium
-      </Link>
-      <div>
-        {userHasValidToken() && (
-          <Link to="/publish">
+      <div className="flex justify-between border-b px-10 py-4">
+        <Link
+          to={'/blogs'}
+          className="text-bold flex cursor-pointer flex-col justify-start text-3xl"
+        >
+          Medium
+        </Link>
+        <div>
+          {userHasValidToken() && (
+            <Link to="/publish">
+              <button
+                type="button"
+                className="me-2 mr-4 mb-2 rounded-full bg-green-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 focus:outline-none"
+              >
+                New
+              </button>
+            </Link>
+          )}
+
+          {userHasValidToken() && (
             <button
               type="button"
-              className="me-2 mr-4 mb-2 rounded-full bg-green-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 focus:outline-none"
+              onClick={() => {
+                localStorage.removeItem('token');
+                toast.success('Logged out successfully', {
+                  autoClose: 2000,
+                });
+                setTimeout(() => {
+                  navigate('/signin');
+                }, 2000);
+              }}
+              className="me-2 mr-4 mb-2 rounded-full bg-gray-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-800 focus:ring-4 focus:ring-green-300 focus:outline-none"
             >
-              New
+              Logout
             </button>
-          </Link>
-        )}
+          )}
 
-        {userHasValidToken() && (
-          <button
-            type="button"
-            onClick={() => {
-              localStorage.removeItem('token');
-              toast.success('Logged out successfully', {
-                autoClose: 2000,
-              });
-              setTimeout(() => {
-                navigate('/signin');
-              }, 2000);
-            }}
-            className="me-2 mr-4 mb-2 rounded-full bg-gray-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-800 focus:ring-4 focus:ring-green-300 focus:outline-none"
-          >
-            Logout
-          </button>
-        )}
-
-        <Avatar size={'big'} name="spandan" />
+          <Avatar size={'big'} name="spandan" />
+        </div>
       </div>
     </div>
   );
